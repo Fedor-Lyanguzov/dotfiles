ebf03ef705f6b2c167aa1746c1089d132a8da7d8
Fedor Lyanguzov <fedor.lyanguzov@ya.ru>
2018-03-30 15:58:11 +0300

    30.03.2018 Удален двойной запуск unclutter

--- .oh-my-zsh/custom/aliases.zsh
+++ .oh-my-zsh/custom/aliases.zsh
@@ -2,4 +2,5 @@ alias open=xdg-open
 alias aliases="$EDITOR $ZSH/custom/aliases.zsh"
 alias vimrc="$EDITOR ~/.vimrc"
 alias venv="source .venv/bin/activate"
+alias mkvenv="python3 -m venv .venv && source .venv/bin/activate"
 alias gcd='git checkout development'
