47d171af7d0834f0c73a4b557226b963f06d62bf
Fedor Lyanguzov <fedor.lyanguzov@ya.ru>
2018-06-21 11:38:30 +0300

    21.06.2018

--- .oh-my-zsh/custom/aliases.zsh
+++ .oh-my-zsh/custom/aliases.zsh
@@ -5,3 +5,19 @@ alias venv="source .venv/bin/activate"
 alias mkvenv="python3 -m venv .venv && source .venv/bin/activate"
 alias gcd='git checkout development'
 alias here=gnome-terminal
+alias sgrep='grep -R -n -H -C 5 --exclude-dir={.git,.svn,CVS,.venv} '
+
+function commands() {
+  awk '{a[$2]++}END{for(i in a){print a[i] " " i}}'
+}
+alias topten="history | commands | sort -rn | head"
+
+function _show_answers() {
+	for f in *; do
+		echo '-------------------- ' $f ' ----------------------'
+		cat $f
+	done
+	echo '------------------------------------------'
+}
+
+alias show_answers="_show_answers | less"
